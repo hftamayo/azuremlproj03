ef1b79782c570997723a17951c891b2d1d0f4326
Herbert Fernandez Tamayo <hftamayo@gmail.com>
2021-01-25 07:56:58 -0600

    adjusting hyperdrive experiment in webservice deployment

--- score.py
+++ score.py
@@ -8,7 +8,9 @@ import joblib, pickle
 
 def init():
     global model
-    model_path = Model.get_model_path('outputs/bhypermodel.joblib')
+    #model_path = Model.get_model_path('outputs/bhypermodel.joblib')
+    model_path = Model.get_model_path('HyperDrive_HighAccuracy')
+    #optional: model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'bhypermodel.joblib')
     model = joblib.load(model_path)
 
 
